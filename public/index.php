e44c10c2a512928fd3cc6a2324561f65b2ef08e9
jeiya1 <aniagjoseph593@gmail.com>
2026-03-24 06:10:10 +0800

    feat: persistent user account

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+$lifetime = 60 * 60 * 24 * 30; // 30 days
+
+session_set_cookie_params([
+    'lifetime' => $lifetime,
+    'path' => '/',
+    'secure' => false,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
 session_start();
 
 require_once '../config/connect.php';
